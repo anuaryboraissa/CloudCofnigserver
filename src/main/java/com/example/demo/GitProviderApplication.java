71ca248a587019062d7519de6272eda3428e018f
mbadddy <102548370+mbadddy@users.noreply.github.com>
2022-03-28 13:12:29 -0700

    First commit

---
+++ src/main/java/com/example/demo/GitProviderApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/provider")
+public class GitProviderApplication {
+	@GetMapping("/data")
+    public List<String> getList() {
+		return Stream.of("Anuary","issa","Iddii").collect(Collectors.toList());
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(GitProviderApplication.class, args);
+	}
+
+}
